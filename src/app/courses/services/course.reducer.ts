46cb208270a4a509713dc7dc40974f88db7d7cc4
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-06-03 08:12:46 -0700

    learning about entity

--- src/app/courses/services/course.reducer.ts
+++ src/app/courses/services/course.reducer.ts
@@ -1,6 +1,9 @@
 import { Course } from '../model/course';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 
-export interface CourseState {
-  coursesEntities: { [key: number]: Course };
-  coursesOrder: number[];
+export interface CourseState extends EntityState<Course> {
+  // coursesEntities: { [key: number]: Course };
+  // coursesOrder: number[];
 }
+
+export const adapter: EntityAdapter<Course> = createEntityAdapter<Course>();
